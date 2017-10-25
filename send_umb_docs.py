548005770dadd48f7ee3694033f9d0158d07d551
Mike Bonnet <mikeb@redhat.com>
2017-10-25 19:03:16 -0400

    use an absolute path

--- send_umb_docs.py
+++ send_umb_docs.py
@@ -1,6 +1,9 @@
 
 from flask import send_from_directory
+import os.path
 
+@app.route('/umb/', defaults={'path': 'index.html'})
 @app.route('/umb/<path:path>')
 def send_umb_docs(path):
-    return send_from_directory('umb', path)
+    dirpath = os.path.dirname(__file__) + '/umb'
+    return send_from_directory(dirpath, path)
