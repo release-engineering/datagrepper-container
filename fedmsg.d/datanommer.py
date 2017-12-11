dc448a16390579e3d229850615516a588655b663
Mike Bonnet <mikeb@redhat.com>
2017-12-11 17:51:26 -0500

    remove unnecessary datanommer config

--- fedmsg.d/datanommer.py
+++ fedmsg.d/datanommer.py
@@ -6,10 +6,9 @@ DB_USERNAME = os.environ['DB_USERNAME']
 DB_PASSWORD = os.environ['DB_PASSWORD']
 
 config = {
-    'datanommer.enabled': True,
+    'datanommer.enabled': False,
     'datanommer.sqlalchemy.url': 'postgresql://{0}:{1}@{2}/{3}'.format(DB_USERNAME,
                                                                        DB_PASSWORD,
                                                                        DB_HOST,
                                                                        DB_NAME),
-    'datanommer.topic': os.environ['DATANOMMER_TOPIC'],
 }
