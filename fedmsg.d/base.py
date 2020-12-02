883de1916778c2a2a8ed75eea3ea53eb28cba683
Mike Bonnet <mikeb@redhat.com>
2020-12-01 16:22:54 -0800

    make Super-Linter happy

--- fedmsg.d/base.py
+++ fedmsg.d/base.py
@@ -1,7 +1,7 @@
 import os
 
 config = dict(
-    environment=os.environ['ENVIRONMENT'],
+    environment=os.environ["ENVIRONMENT"],
     zmq_enabled=False,
     endpoints={},
     validate_signatures=False,
