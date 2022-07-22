2f8fc46ed19dd175b52753e99ad2b97909a2f876
Ralph Bean <rbean@redhat.com>
2022-07-22 15:51:14 -0400

    Remove all content
    See https://gitlab.cee.redhat.com/exd/catalog/-/merge_requests/4195

--- fedmsg.d/base.py
+++
@@ -1,8 +0,0 @@
-import os
-
-config = dict(
-    environment=os.environ["ENVIRONMENT"],
-    zmq_enabled=False,
-    endpoints={},
-    validate_signatures=False,
-)
--- fedmsg.d/datagrepper.py
+++
@@ -1,8 +0,0 @@
-config = {
-    "content_security_policy": "connect-src https://*.redhat.com wss://*.redhat.com",
-    "datagrepper_logo": "static/datagrepper-debranded.png",
-    "theme_css_url": "static/pnt-bootstrap.min.css",
-    "message_bus_docs": "https://mojo.redhat.com/docs/DOC-1072237",
-    "message_bus_shortname": "UMB",
-    "message_bus_longname": "Unified Message Bus (UMB)",
-}
